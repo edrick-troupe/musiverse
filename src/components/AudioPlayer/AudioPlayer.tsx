ba34849abaa7c1e5bfa07d2d25a72a2e9b3fb8c7
Edrick101 <alphonse.61000@gmail.com>
2023-11-13 18:28:08 +0100

    Main components

---
+++ src/components/AudioPlayer/AudioPlayer.tsx
@@ -0,0 +1,9 @@
+import './AudioPlayer.scss';
+
+export default function AudioPlayer() {
+  return (
+    <div id="audio-player" className="">
+      <h1>AudioPlayer</h1>
+    </div>
+  );
+}
---
+++ src/views/Playlist/Playlist.tsx
@@ -0,0 +1,7 @@
+export default function Playlist() {
+  return (
+    <div id="playlist" className="">
+      <h1>Playlist</h1>
+    </div>
+  );
+}
