b5b531f332545a80001cc53f9088a5999230ff2f
Edrick101 <alphonse.61000@gmail.com>
2023-11-13 18:44:38 +0100

    Homepage layout

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,7 +1,38 @@
+import { useNavigate } from 'react-router-dom';
+
+import Lottie from 'lottie-react';
+import flyingAstronaut from '@assets/lottie/flying-astronaut.json';
+
 export default function Home() {
+  const navigate = useNavigate();
+
   return (
-    <div id="home" className="">
-      <h1>Home</h1>
+    <div
+      id="home"
+      className="flex h-full flex-col items-center justify-evenly py-space-y-sm text-center"
+    >
+      <div id="headlines">
+        <h1>Musiverse</h1>
+        <h2>Découvrez mon univers musical !</h2>
+      </div>
+      <div id="home__animation">
+        <Lottie
+          animationData={flyingAstronaut}
+          style={{
+            height: '50vh',
+            width: 'fit-content',
+          }}
+        />
+      </div>
+      <div>
+        <button
+          id="getting-started"
+          type="button"
+          onClick={() => navigate('/playlist')}
+        >
+          Accéder à la playlist
+        </button>
+      </div>
     </div>
   );
 }
