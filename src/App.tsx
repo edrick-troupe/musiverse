678e8f6fe08d3b54f33d2a946a62b4b3a74f8184
Edrick101 <alphonse.61000@gmail.com>
2023-11-13 18:32:49 +0100

    Routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
 import './App.scss';
+
+import Home from './views/Home/Home';
+import Playlist from './views/Playlist/Playlist';
+import AudioPlayer from './components/AudioPlayer/AudioPlayer';
 
 function App() {
   return (
-    <div id="app" className="font-text">
-      <h1>Hello world !</h1>
+    <div id="app">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/playlist">
+          <Route index element={<Playlist />} />
+          <Route path="track/:id" element={<AudioPlayer />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
