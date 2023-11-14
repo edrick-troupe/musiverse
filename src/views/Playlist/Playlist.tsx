a1a28cb87f59011958430ed9467aac3830613985
Edrick101 <alphonse.61000@gmail.com>
2023-11-14 15:02:18 +0100

    Playlist page: layout and styling

--- src/views/Playlist/Playlist.tsx
+++ src/views/Playlist/Playlist.tsx
@@ -1,3 +1,7 @@
+import { Link } from 'react-router-dom';
+
+import './Playlist.scss';
+
 import { PlaylistData } from '@customTypes/Playlist';
 
 interface PlaylistProps {
@@ -20,8 +24,41 @@ export default function Playlist({
   }
 
   return (
-    <div id="playlist" className="">
-      <h1>Playlist</h1>
+    <div id="playlist" className="flex h-full flex-col">
+      <h3 className="mx-auto w-10/12 py-space-y-xs text-center font-subheadline">
+        Sélectionnez une musique pour en écouter un extrait
+      </h3>
+      <div
+        id="playlist__container"
+        className="mx-auto flex w-10/12 grow flex-col justify-center py-space-y-xs"
+      >
+        <ul className="grid gap-x-space-x-sm gap-y-space-y-xs lg:grid-cols-2 lg:gap-y-space-y-xs xl:grid-cols-3 xl:gap-y-space-y-sm">
+          {playlist.map((track) => (
+            <li key={track.id}>
+              <Link
+                to={`/playlist/track/${track.id}`}
+                className="px-space-x-xs py-space-y-xxs"
+              >
+                <div className="track flex h-full max-h-28 md:max-h-44">
+                  <div className="track__album-cover w-2/5 shrink-0">
+                    <img
+                      src={track.thumbnail}
+                      alt="Track cover"
+                      className="h-full w-full object-cover"
+                    />
+                  </div>
+                  <div className="track__info px-3 pt-4">
+                    <h4 className="font-title font-bold">
+                      <strong>{track.title}</strong>
+                    </h4>
+                    <h5 className="">{track.artist}</h5>
+                  </div>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
